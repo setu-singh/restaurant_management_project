c93991698ba1a1090e5a69ad940ab682f219969d
setu-singh <139809038+setu-singh@users.noreply.github.com>
2025-08-14 09:10:20 +0530

    32507

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -1,4 +1,4 @@
-Add urlpatterns:  path('', views.home, name='home')
+Add urlpatterns:  path(, views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
