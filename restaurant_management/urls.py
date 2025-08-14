679d75433d35016cb060c16022ddd636061d1c20
setu-singh <139809038+setu-singh@users.noreply.github.com>
2025-08-14 09:02:06 +0530

    32495

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -1,5 +1,4 @@
-
-    1. Add an import:  from my_app import views
+1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
