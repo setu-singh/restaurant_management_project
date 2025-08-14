1ecae2a7ce5d6c21524e805c3bc1120f25da9513
setu-singh <139809038+setu-singh@users.noreply.github.com>
2025-08-14 08:58:38 +0530

    32486

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -1,5 +1,3 @@
-URL configuration for restaurant_management project.
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
