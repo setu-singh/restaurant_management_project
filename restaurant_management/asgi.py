866ccf6a5862add035336043fdffa18fa1cd5a4d
setu-singh <139809038+setu-singh@users.noreply.github.com>
2025-08-17 10:18:09 +0530

    34416

--- restaurant_management/asgi.py
+++ restaurant_management/asgi.py
@@ -4,5 +4,5 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurant_management.settings')
 
-application = get_asgi_application()..
-;
+application = get_asgi_application().
+;;
