43a6293ea47f49a224731546007ef59cdbcbc64d
setu-singh <139809038+setu-singh@users.noreply.github.com>
2025-08-26 20:34:37 +0530

    39218

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s^x_u!itekxd=@a3o3zv4d%hw$j6d#2v358w
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = Tru
 
 ALLOWED_HOSTS = ['*']
 
